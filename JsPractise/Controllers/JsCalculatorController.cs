f0e91707d4905fc80d7a5acccc4d3c6fef5b3620
Rowan Brouwer <Rowan.Brouwer@itvitaelearning.nl>
2021-08-10 15:14:50 +0200

    Add project files.

---
+++ JsPractise/Controllers/JsCalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsPractise.Controllers
+{
+    public class JsCalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JsPractise/Scripts/JsCalculator.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('keypress', event => {
+    let x = event.keyCode;
+    if (x >= 48 && x <= 57)
+    {
+        display(String.fromCharCode(x));
+    }
+    else if (event.key === '/' || event.key === '+' || event.key === '-' || event.key === '*' || event.key === '.')
+    {
+        display(event.key);
+    }
+    else if (x === 13)
+    {
+        calculate();
+    }
+    else if (event.key === 'c')
+    {
+        clr();
+    }
+});
+
+function display(val)
+{
+    document.getElementById("textval").value += val;
+}
+
+function calculate()
+{
+    let x = document.getElementById("textval").value;
+    let y = eval(x);
+    document.getElementById("textval").value = y;
+}
+
+function clr()
+{
+    document.getElementById("textval").value = "";
+}
